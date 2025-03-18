3eaa54ad063b626fb6da5ca4b8c93d02e6b5f634
parakh2001 <parakh.gupta2571@gmail.com>
2025-03-18 13:27:14 +0530

    added table

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { Navbar } from "./components/Navbar";
+import { Tablelist } from "./components/Tablelist";
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <Tablelist />
+    </div>
+  );
+}
+export default App
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+export const Navbar = () => {
+    return (
+        <div className="navbar bg-base-100 shadow-sm">
+            <div className="navbar-start">
+                <a className="btn btn-ghost text-xl">Clients</a>
+            </div>
+            <div className="flex gap-2">
+                <input type="text" placeholder="Search" className="input input-bordered w-48 md:w-auto" />
+            </div>
+            <div className="navbar-center">
+            </div>
+            <div className="navbar-end">
+                <a className="btn btn-primary">Button</a>
+            </div>
+        </div>
+    );
+}
